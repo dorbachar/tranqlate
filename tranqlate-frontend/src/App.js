b4184bbbd25d84c26d71278e4305503a3a98ee69
dor.bachar <dor.bachar@taboola.com>
2022-12-10 14:14:10 +0200

    Revert "getting started with a little elements and state"
    This reverts commit 52cbf801bbb214331cf77d56dcf37d51a1c4304e.

--- tranqlate-frontend/src/App.js
+++ tranqlate-frontend/src/App.js
@@ -2,12 +2,11 @@ import './App.css';
 import {Route, Redirect, Switch, BrowserRouter} from "react-router-dom";
 import {NotFound} from "./pages/NotFound";
 import {Translate} from "./pages/Translate";
-import styles from "./styles/general.module.scss"
 
 function App() {
     return (
         <>
-            <header className={styles["title"]}>
+            <header>
                 <h1>tranQlate</h1>
             </header>
             <main>
--- tranqlate-frontend/src/pages/Translate.js
+++ tranqlate-frontend/src/pages/Translate.js
@@ -1,29 +1,7 @@
-import {useState} from "react";
-
 export function Translate() {
-    const [text, setText] = useState("");
-    const [translation, setTranslation] = useState("");
     return (
         <>
-            <form onSubmit={e => {
-                e.preventDefault();
-                console.log(translation);
-                setText("");
-                setTranslation("Trash");
-            }}>
-                <input type="text"
-                       placeholder=""
-                       value={text}
-                       onChange={
-                           (e) => {
-                               setText(e.target.value);
-                               setTranslation("");
-                           }
-                       }
-                />
-                <button type="submit">Translate</button>
-            </form>
-            <p>{translation}</p>
+            <p>This is the translate page</p>
         </>
     )
 }
