52cbf801bbb214331cf77d56dcf37d51a1c4304e
dor.bachar <dor.bachar@taboola.com>
2022-12-10 14:08:37 +0200

    getting started with a little elements and state

--- tranqlate-frontend/src/pages/Translate.js
+++ tranqlate-frontend/src/pages/Translate.js
@@ -1,7 +1,29 @@
+import {useState} from "react";
+
 export function Translate() {
+    const [text, setText] = useState("");
+    const [translation, setTranslation] = useState("");
     return (
         <>
-            <p>This is the translate page</p>
+            <form onSubmit={e => {
+                e.preventDefault();
+                console.log(translation);
+                setText("");
+                setTranslation("Trash");
+            }}>
+                <input type="text"
+                       placeholder=""
+                       value={text}
+                       onChange={
+                           (e) => {
+                               setText(e.target.value);
+                               setTranslation("");
+                           }
+                       }
+                />
+                <button type="submit">Translate</button>
+            </form>
+            <p>{translation}</p>
         </>
     )
 }
