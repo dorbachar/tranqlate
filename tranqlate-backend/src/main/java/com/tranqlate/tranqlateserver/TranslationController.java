fd1b216cdcc3c9da239cbefd9ec67cd99a232fe2
dor.bachar <dor.bachar@taboola.com>
2023-01-15 10:14:30 +0200

    getLanguages endpoint

--- tranqlate-backend/src/main/java/com/tranqlate/tranqlateserver/TranslationController.java
+++ tranqlate-backend/src/main/java/com/tranqlate/tranqlateserver/TranslationController.java
@@ -1,34 +1,28 @@
 package com.tranqlate.tranqlateserver;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+import lombok.AllArgsConstructor;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.google.auth.Credentials;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.translate.Translate;
-import com.google.cloud.translate.TranslateOptions;
-import com.google.cloud.translate.Translation;
-
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
+@AllArgsConstructor
+@Component
 public class TranslationController {
+    private final TranslationService translationService;
 
     @GetMapping("/translate")
-    public String translateText(@RequestParam String input, @RequestParam String sourceLanguage, @RequestParam String targetLanguage) throws IOException {
+    public String translateText(@RequestParam String input, @RequestParam String sourceLanguage, @RequestParam String targetLanguage) {
+        return translationService.translate(input, sourceLanguage, targetLanguage);
+    }
 
-        Credentials credentials = GoogleCredentials.fromStream(new FileInputStream("src/main/resources/tranqlate-dfc9ca7e63b3.json"));
-        Translate translate = TranslateOptions.newBuilder().setCredentials(credentials).build().getService();
-        Translation translation = translate.translate(input,
-                Translate.TranslateOption.sourceLanguage(sourceLanguage),
-                Translate.TranslateOption.targetLanguage(targetLanguage));
-        System.out.println(translation.getTranslatedText());
-
-        return "{ \"translation\":\"" + translation.getTranslatedText() + "\"}";
+    @GetMapping("/getLanguages")
+    public String getLanguages() {
+        return translationService.getLanguages();
     }
 }
