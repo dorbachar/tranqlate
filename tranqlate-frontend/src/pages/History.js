0d4384c8b5f54371fb592e178d3785fee82bef87
dor.bachar <dor.bachar@taboola.com>
2023-02-27 19:03:02 +0200

    history displaying in table

--- tranqlate-frontend/src/pages/History.js
+++ tranqlate-frontend/src/pages/History.js
@@ -1,12 +1,45 @@
 import {StorageContext} from "../providers/StorageContextProvider";
 import {useContext} from "react";
+import {Table, TableCaption, TableContainer, Tbody, Td, Tfoot, Th, Thead, Tr} from "@chakra-ui/react";
+import {get} from "lodash/fp";
+import {keys} from "lodash";
+import {first} from "lodash/array";
 
 export function History() {
     const {
         translationsHistory
     } = useContext(StorageContext)
 
-    return translationsHistory.map((value) => {
-        return <p>{JSON.stringify(value)}</p>;
-    })
+    return (<TableContainer>
+            <Table variant='simple' colorScheme='blue'>
+                <TableCaption placement="top">Translations History</TableCaption>
+                <Thead>
+                    <Tr background="#f5fcfc">
+                        {
+                            keys(first(translationsHistory)).map(key => <Td fontWeight="bold"> {key} </Td>)
+                        }
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    {
+                        translationsHistory.map(translation => {
+                            return <Tr>
+                                {
+                                    Object.values(translation).map(value => <Td> {value} </Td>)
+                                }
+                            </Tr>;
+                        })
+                    }
+
+                </Tbody>
+                {/*<Tfoot>*/}
+                {/*    <Tr>*/}
+                {/*        <Th>To convert</Th>*/}
+                {/*        <Th>into</Th>*/}
+                {/*        <Th isNumeric>multiply by</Th>*/}
+                {/*    </Tr>*/}
+                {/*</Tfoot>*/}
+            </Table>
+        </TableContainer>
+    )
 }
